ede4d7c5268b1bac4c8939e45eca9a9a813b5ea2
emran40989 <emran40989@gmail.com>
2026-01-23 12:13:44 +0600

    data stored to mongodb successfully

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const uri =
   "mongodb+srv://emran40989:2WCaL7vfRzutP1hB@cluster0.oh3nqax.mongodb.net/?appName=Cluster0";
@@ -22,9 +23,16 @@ async function run() {
   try {
     await client.connect();
 
+    const database = client.db('pet_service');
+    const petService = database.collection('services')
 
     app.post('/services', async (req, res) => {
-
+      const data = req.body;
+      console.log(data);
+      const result = await petService.insertOne(data);
+      res.send(result);
+      
+      
     });
 
     await client.db("admin").command({ ping: 1 });
